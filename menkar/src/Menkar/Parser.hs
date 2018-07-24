21c426eb6e726827b7c50ea7d1405e6e4412d952
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2018-07-24 15:33:10 +0200

    Basic parsing operations. Subdivision of characters.

--- menkar/src/Menkar/Parser.hs
+++ menkar/src/Menkar/Parser.hs
@@ -8,7 +8,10 @@ import qualified Text.Megaparsec.Expr as MP
 import qualified Menkar.Raw as Raw
 import qualified Text.Megaparsec.Char.Lexer as MPL
 import Control.Monad.Identity
+import Data.Char
 --import Data.Ord
+
+-- ParseError ----------------------------------------------
 
 data ParseError
 instance Eq ParseError where
@@ -18,16 +21,53 @@ instance Ord ParseError where
 instance Show ParseError where
   show e = "ERROR"
 
+-- CanParse ------------------------------------------------
+
 class (MP.MonadParsec ParseError String m) => CanParse m
 
 type TestParser = MP.Parsec ParseError String
 instance CanParse (MP.ParsecT ParseError String Identity)
 
+-- characters ----------------------------------------------
+
+data CharType =
+  SpaceChar | LetterChar | DigitChar | UnderscoreChar | OpChar | OpenChar | CloseChar
+  deriving (Show, Eq)
+
+charType :: Char -> CharType
+charType c
+  | isSpace c = SpaceChar
+  | isDigit c = DigitChar
+  | isLetter c = LetterChar
+  | c == '_' = UnderscoreChar
+  | otherwise = case generalCategory c of
+      OpenPunctuation -> OpenChar
+      ClosePunctuation -> CloseChar
+      _ -> OpChar
+
+-- subparsers ----------------------------------------------
+
+{-| Consumes zero or more whitespace characters, line or block comments -}
 manySpace :: CanParse m => m ()
 manySpace = MPL.space MP.space1 lineCmnt blockCmnt
   where
     lineCmnt  = MPL.skipLineComment "//"
     blockCmnt = MPL.skipBlockComment "/*" "*/"
+
+{-| 'lexeme p' consumes 'p', then 'manySpace'. -}
+lexeme :: CanParse m => m a -> m a
+lexeme = MPL.lexeme manySpace
+
+{-| Cconsumes and returns the specified string.
+    DO NOT USE THIS FOR RESERVED WORDS, lest "ifbla" will be parsed as "if bla". -}
+symbol :: CanParse m => String -> m String
+symbol = MPL.symbol manySpace
+
+parens :: CanParse m => m a -> m a
+parens = MP.between (symbol "(") (symbol ")")
+
+accols :: CanParse m => m a -> m a
+accols = MP.between (symbol "{") (symbol "}")
 
 --parse :: MonadParser m => m Raw.Module
 --parse = _
