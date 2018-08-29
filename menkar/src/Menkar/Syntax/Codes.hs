716e4cd4aa2b7455033d860dd1a99a1d25a4d566
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2018-08-29 12:21:35 +0200

    Syntax codes: renamed def to op.

--- menkar/src/Menkar/Syntax/Codes.hs
+++ menkar/src/Menkar/Syntax/Codes.hs
@@ -6,44 +6,37 @@ module Menkar.Syntax.Codes where
 import Data.Kind
 import Data.Type.Natural (Nat(..))
 
-{-| @'Term' def '(n, cl) v@ is the type of terms of syntactic class @cl@, built with operators from @def@,
+{-| @'Term' op '(n, cl) v@ is the type of terms of syntactic class @cl@, built with operators from @op@,
     and @n@ free variables from @v@.
 
     Here, @'Nothing@ is the syntactic class that variables are part of, and other classes are of the form @'Just c@.
 
-    Here, @def arityclasses cl@ is the type of operators generating terms of syntactic class @cl@. The length of
+    Here, @op arityclasses cl@ is the type of operators generating terms of syntactic class @cl@. The length of
     @arityclasses@ gives the number of arguments the operator takes. Each entry @'(n, cl)@ states that an argument has
     arity @n@ (can use @n@ additional variables) and should belong to syntactic class @cl@.
 -}
-data Term (def :: [(Nat, Maybe *)] -> Maybe * -> *) (arityclass :: (Nat, Maybe *)) (v :: *) :: * where
-  Var :: v -> Term def '(Z, Nothing) v
-  Abs :: Term def '(arity, cl) (Maybe v) -> Term def '(S arity, cl) v
-  Term :: def arityclasses cl -> Args def arityclasses v -> Term def '(Z, cl) v
+data Term (op :: [(Nat, Maybe *)] -> Maybe * -> *) (arityclass :: (Nat, Maybe *)) (v :: *) :: * where
+  Var :: v -> Term op '(Z, Nothing) v
+  Abs :: Term op '(arity, cl) (Maybe v) -> Term op '(S arity, cl) v
+  Term :: op arityclasses cl -> Args op arityclasses v -> Term op '(Z, cl) v
 
-data Args (def :: [(Nat, Maybe *)] -> Maybe * -> *) (arityclasses :: [(Nat, Maybe *)]) (v :: *) :: * where
-  EndArgs :: Args def '[] v
-  (:..) :: Term def '(arity, cl) v -> Args def arityclasses v -> Args def ('(arity, cl) ': arityclasses) v
+data Args (op :: [(Nat, Maybe *)] -> Maybe * -> *) (arityclasses :: [(Nat, Maybe *)]) (v :: *) :: * where
+  EndArgs :: Args op '[] v
+  (:..) :: Term op '(arity, cl) v -> Args op arityclasses v -> Args op ('(arity, cl) ': arityclasses) v
 
-instance Functor (Term def arityclass) where
+instance Functor (Term op arityclass) where
   fmap f (Var v) = Var $ f v
   fmap f (Abs t) = Abs $ fmap (fmap f) t
   fmap f (Term d args) = Term d (fmap f args)
 
-instance Functor (Args def arityclasses) where
+instance Functor (Args op arityclasses) where
   fmap f EndArgs = EndArgs
   fmap f (arg :.. args) = fmap f arg :.. fmap f args
 
 class Swallows (beast :: * -> *) (food :: * -> *) where
   swallow :: beast (food v) -> beast v
 
-instance Swallows (Term def '(n, cl)) (Term def '(n, Nothing)) where
+instance Swallows (Term op '(n, cl)) (Term op '(n, Nothing)) where
   swallow (Var tv) = tv
   swallow ttv = _
-
-{-
-data OpLambdaApp fs where
-  App :: OpLambdaApp [Identity, Identity]
--}
-
---TODO: use 'Either (r :: *) v' instead of '(f :: Traversable) v'
 
