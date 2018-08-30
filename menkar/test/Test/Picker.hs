e75b1a736e0a0f3cfbe626996da2ced51b285cca
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2018-08-30 17:48:58 +0200

    Testing: trying to make Pickable derivable.

--- menkar/test/Test/Picker.hs
+++ menkar/test/Test/Picker.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE GADTs, DeriveFunctor, StandaloneDeriving, RankNTypes, ApplicativeDo, PartialTypeSignatures #-}
+{-# LANGUAGE GADTs, DeriveFunctor, StandaloneDeriving, RankNTypes, ApplicativeDo, PartialTypeSignatures, TypeOperators,
+DefaultSignatures, DeriveGeneric, FlexibleContexts #-}
 {-# OPTIONS_GHC -Wno-partial-type-signatures #-}
 
 module Test.Picker where
@@ -10,6 +11,8 @@ import Data.Number.Nat (Nat(..), take)
 import GHC.Generics
 import Data.Maybe
 import Control.Monad.Trans.Maybe
+import Control.Applicative
+import Control.Monad
 
 class Monad m => MonadPicker m where
   -- | Pick a random element.
@@ -35,19 +38,28 @@ instance Applicative Picker where
   pure a = Picker $ \ d -> runMaybeT $ return a
   Picker f <*> Picker a = Picker $ \ d -> runMaybeT $ MaybeT (f d) <*> MaybeT (a d)
 
+instance Alternative Picker where
+  empty = Picker $ \d -> state $ \g -> (Nothing, g)
+  pa <|> pb = choose [pa, pb]
+
 instance Monad Picker where
   Picker a >>= f = Picker $ \ d -> runMaybeT $ do
     a' <- MaybeT $ a d
     let Picker fa = f a'
     MaybeT $ fa d
-  fail msg = Picker $ \d -> state $ \g -> (Nothing, g)
+  fail msg = empty
+
+instance MonadPlus Picker where
+  mzero = empty
+  mplus = (<|>)
 
 instance MonadPicker Picker where
   runPicker (Picker a) = a
+  choose [] = empty
   choose mas = Picker $
     \d -> runMaybeT $ (MaybeT $ runThePicker 0) >>= (\r -> MaybeT $ runPicker (mas !! (r `mod` length mas)) d)
   chooseDeeper mas mbs = Picker $
-    \d -> if d == 0
+    \d -> if d <= 0
              then runPicker (choose mas :: Picker _) 0
              else runPicker (choose $ mas ++ mbs :: Picker _) (d - 1)
 
@@ -55,10 +67,12 @@ instance MonadPicker Picker where
 
 class Pickable a where
   picker :: Picker a
-  picker = Picker $ runThePicker
+  default picker :: (Generic a, Pickable (Rep a ())) => Picker a
+  picker = to <$> (picker :: Picker (Rep a ())) :: Picker a
+  --picker = Picker $ runThePicker
   runThePicker :: RandomGen g => {-| maximal search depth -} Nat -> State g (Maybe a)
   runThePicker = runPicker picker
-  {-# MINIMAL picker | runThePicker #-}
+  {- # MINIMAL picker | runThePicker #-}
 
 pick :: (Pickable a, RandomGen g) => g -> Nat -> Maybe a
 pick g d = evalPicker picker g d
@@ -68,7 +82,7 @@ sample n g d = samplePicker n picker g d
 ------------------------------------
 
 instance Pickable Int where
-  runThePicker d = Just <$> state next
+  picker = Picker $ \d -> Just <$> state next
 
 instance Pickable () where
   picker = return ()
@@ -83,3 +97,20 @@ instance (Pickable a) => Pickable (Maybe a) where
   picker = choose $ return Nothing : (take 4 $ repeat $ Just <$> picker)
 
 -----------------------------------
+
+instance Pickable (V1 p) where
+  picker = empty
+instance Pickable (U1 p) where
+  picker = return U1
+instance Pickable p => Pickable (Par1 p) where
+  picker = Par1 <$> picker
+instance Pickable (f p) => Pickable (Rec1 f p) where
+  picker = Rec1 <$> picker
+instance Pickable c => Pickable (K1 i c p) where
+  picker = K1 <$> picker
+instance Pickable (f p) => Pickable (M1 i c f p) where
+  picker = M1 <$> picker
+instance (Pickable (f p), Pickable (g p)) => Pickable ((f :+: g) p) where
+  picker = (L1 <$> picker) <|> (R1 <$> picker)
+instance (Pickable (f p), Pickable (g p)) => Pickable ((f :*: g) p) where
+  picker = (:*:) <$> picker <*> picker
