6d621a3ea880a7c6e0e3bec800f703174220cb36
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2018-09-14 18:05:30 +0200

    AllocT monad transformer.

--- menkar/src/Control/Categorical/Monad/Alloc.hs
+++ menkar/src/Control/Categorical/Monad/Alloc.hs
@@ -71,3 +71,64 @@ test b = runAlloc $
      Const False -> return1 U1
   ) >>=! \(r1 :*: U1) ->
   readRef r1
+
+---------------------------------------
+
+newtype AllocT m a s = AllocT {getAllocT :: s -> m (OutAlloc a s)}
+
+instance (Functor m) => C.Functor (AllocT m) (C.NT (->)) (C.NT (->)) where
+  fmap (C.NT f) = C.NT $ \ (AllocT comp) -> AllocT $ \ s0 -> (<$> comp s0) $ \case
+    OutAlloc s1 a lens -> OutAlloc s1 (f a) lens
+
+instance (Monad m) => C.Monad (AllocT m) (C.NT (->)) where
+  return = C.NT $ \ a -> AllocT $ \ s0 -> return $ OutAlloc s0 a id
+  bind (C.NT f) = C.NT $ \ (AllocT comp) -> AllocT $ \ s0 -> comp s0 >>= \ case
+    OutAlloc s1 a lens1 -> (<$> getAllocT (f a) s1) $ \ case
+      OutAlloc s2 b lens2 -> OutAlloc s2 b (lens2 . lens1)
+
+lift1 :: (Functor m) => m (a s) -> AllocT m a s
+lift1 ma = AllocT $ \ s0 -> (\ a -> OutAlloc s0 a id) <$> ma
+
+runAllocTWith :: (Functor m) => s -> AllocT m (Const a) s -> m a
+runAllocTWith s0 (AllocT comp) = (<$> comp s0) $ \ case
+  OutAlloc s1 (Const a) lens -> a
+
+runAllocT :: (Functor m) => AllocT m (Const a) () -> m a
+runAllocT = runAllocTWith ()
+
+allocT :: (Applicative m) => a -> AllocT m (AllocRef a) s
+allocT a = AllocT $ \ s0 -> pure $ OutAlloc (s0, a) (AllocRef _2) _1
+
+modifyAllT :: (Applicative m) => (s -> s) -> AllocT m U1 s
+modifyAllT f = AllocT $ \ s0 -> pure $ OutAlloc (f s0) U1 id
+modifyRefT :: (Applicative m) => AllocRef a s -> (a -> a) -> AllocT m U1 s
+modifyRefT (AllocRef lens) g = modifyAllT $ over lens $ g
+
+readAllT :: (Applicative m) => AllocT m (Const s) s
+readAllT = AllocT $ \ s0 -> pure $ OutAlloc s0 (Const s0) id
+readRefT :: (Applicative m) => AllocRef a s -> AllocT m (Const a) s
+readRefT (AllocRef lens) = AllocT $ \ s0 -> pure $ OutAlloc s0 (Const $ view lens s0) id
+
+rememberAllT :: (Functor m) => AllocT m a s -> AllocT m (AllocRef s :*: a) s
+rememberAllT (AllocT comp) = AllocT $ \ s0 -> (<$> comp s0) $ \ case
+  OutAlloc s1 a lens -> OutAlloc s1 (AllocRef lens :*: a) lens
+
+remember1T :: (Functor m) => AllocRef x s -> AllocT m a s -> AllocT m (AllocRef x :*: a) s
+remember1T (AllocRef lens0) (AllocT comp) = AllocT $ \ s0 -> (<$> comp s0) $ \ case
+  OutAlloc s1 a lens1 -> OutAlloc s1 (AllocRef (lens1 . lens0) :*: a) lens1
+
+-----------------
+
+testT :: [Int]
+testT = runAllocT $
+  lift1 [Const True, Const False] >>=! \ (Const b) ->
+  allocT 5 >>=! \r1 ->
+  (remember1T r1 $ return1 $ Const b) >>=! \(r1 :*: y) ->
+  (remember1T r1 $ case y of
+     Const True ->
+       (remember1T r1 $ allocT 7) >>=! \(r1 :*: r2) ->
+       (remember1T r1 $ readRefT r2) >>=! \(r1 :*: Const x) ->
+       modifyRefT r1 (x +)
+     Const False -> return1 U1
+  ) >>=! \(r1 :*: U1) ->
+  readRefT r1
