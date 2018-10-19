0305ea25fdb0afc406a495425aca749c5fb3ef84
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2018-10-19 14:30:03 +0200

    Menkar.Fine.WHNormalize : adapted to new context structure.

--- menkar/src/Menkar/Fine/WHNormalize.hs
+++ menkar/src/Menkar/Fine/WHNormalize.hs
@@ -3,6 +3,7 @@ module Menkar.Fine.WHNormalize where
 import Menkar.Fine.Syntax
 import Menkar.Fine.Context
 import Menkar.Fine.LookupQName
+import Menkar.Fine.Multimode
 import Data.Void
 import Control.Monad.Writer
 import Control.Exception.AssertFalse
@@ -17,21 +18,19 @@ import Control.Exception.AssertFalse
 -}
 --TODOMOD normalize tmFst in different context
 --TODOMOD normalize unboxed term in different context
-whnormalizeElim :: (Functor mode, Functor modty, CanSwallow (Term mode modty) mode, CanSwallow (Term mode modty) modty) =>
+whnormalizeElim :: Multimode mode modty =>
   Ctx Type mode modty v Void ->
-  mode v {-^ eliminee's mode -} ->
-  mode v {-^ result mode -} ->
-  modty v ->
+  ModedModality mode modty v {-^ how eliminee is used -} ->
   Term mode modty v {-^ eliminee -} ->
   Eliminator mode modty v ->
   Writer [Int] (Term mode modty v)
-whnormalizeElim gamma d1 d2 mu eliminee e = do
-  whnEliminee <- whnormalize ((VarFromCtx <$> ModedContramodality d2 mu) :\\ gamma) d1 eliminee
+whnormalizeElim gamma dmu eliminee e = do
+  whnEliminee <- whnormalize ((VarFromCtx <$> dmu) :\\ gamma) eliminee
   case whnEliminee of
-    Var3 v -> return $ Expr3 $ TermElim (ModedModality d1 mu) (Var3 v) e
-    Expr3 (TermMeta _ _) -> return $ Expr3 $ TermElim (ModedModality d1 mu) whnEliminee e
+    Var3 v -> return $ Expr3 $ TermElim dmu (Var3 v) e
+    Expr3 (TermMeta _ _) -> return $ Expr3 $ TermElim dmu whnEliminee e
       -- careful with glue/weld!
-    Expr3 (TermProblem _) -> return $ Expr3 $ TermElim (ModedModality d1 mu) whnEliminee e
+    Expr3 (TermProblem _) -> return $ Expr3 $ TermElim dmu whnEliminee e
     Expr3 (TermCons t) -> case (t, e) of
       {- -Hofmann-Streicher universe code case
       (ConsUniHS d1' typeTerm, ElimUnsafeResize) -> return $ Expr3 $ TermCons $ ConsUniHS d1' $ case typeTerm of
@@ -45,44 +44,42 @@ whnormalizeElim gamma d1 d2 mu eliminee e = do
               VarWkn w -> Var3 w
               VarLast -> arg
               _ -> unreachable
-        in whnormalize gamma d2 (join $ subst <$> binding'body binding)
+        in whnormalize gamma (join $ subst <$> binding'body binding)
       --sigma cases
-      (Pair sigmaBinding tmFst tmSnd, Fst sigmaBinding') -> whnormalize gamma d2 tmFst
-      (Pair sigmaBinding tmFst tmSnd, Snd sigmaBinding') -> whnormalize gamma d2 tmSnd
+      (Pair sigmaBinding tmFst tmSnd, Fst sigmaBinding') -> whnormalize gamma tmFst
+      (Pair sigmaBinding tmFst tmSnd, Snd sigmaBinding') -> whnormalize gamma tmSnd
       (Pair sigmaBinding tmFst tmSnd, ElimPair motive clause) ->
         let subst v = case v of
               VarWkn (VarWkn w) -> Var3 w
               VarWkn VarLast -> tmFst
               VarLast -> tmSnd
               _ -> unreachable
-        in whnormalize gamma d2 (join $ subst <$> binding'body (binding'body clause))
+        in whnormalize gamma (join $ subst <$> binding'body (binding'body clause))
       --empty type cases (none)
       --unit cases (none)
       --box cases
-      (ConsBox seg tm, Unbox seg') -> whnormalize gamma d2 tm
+      (ConsBox seg tm, Unbox seg') -> whnormalize gamma tm
       --nonsensical cases
-      (_, _) -> return $ Expr3 $ TermProblem $ Expr3 $ TermElim (ModedModality d1 mu) whnEliminee e
+      (_, _) -> return $ Expr3 $ TermProblem $ Expr3 $ TermElim dmu whnEliminee e
     Expr3 _ -> unreachable
 
-whnormalizeNV :: (Functor mode, Functor modty, CanSwallow (Term mode modty) mode, CanSwallow (Term mode modty) modty) =>
+whnormalizeNV :: Multimode mode modty =>
   Ctx Type mode modty v Void ->
-  mode v ->
   TermNV mode modty v ->
   Writer [Int] (Term mode modty v)
-whnormalizeNV gamma d t@(TermCons _) = return . Expr3 $ t   -- Mind glue and weld!
-whnormalizeNV gamma d (TermElim dmu t e) = whnormalizeElim gamma (modality'dom dmu) d (modality'mod dmu) t e
-whnormalizeNV gamma d t@(TermMeta i depcies) = Expr3 t <$ tell [i]
-whnormalizeNV gamma d (TermQName qname) = case lookupQNameTerm gamma qname of
+whnormalizeNV gamma t@(TermCons _) = return . Expr3 $ t   -- Mind glue and weld!
+whnormalizeNV gamma (TermElim dmu t e) = whnormalizeElim gamma dmu t e
+whnormalizeNV gamma t@(TermMeta i depcies) = Expr3 t <$ tell [i]
+whnormalizeNV gamma (TermQName qname) = case lookupQNameTerm gamma qname of
   Nothing -> return $ Expr3 $ TermProblem $ Expr3 $ TermQName qname
-  Just t -> whnormalize gamma d (unVarFromCtx <$> t)
-whnormalizeNV gamma d (TermSmartElim eliminee eliminators result) = whnormalize gamma d result
-whnormalizeNV gamma d (TermGoal str result) = whnormalize gamma d result
-whnormalizeNV gamma d t@(TermProblem _) = return $ Expr3 t
+  Just leftDividedTerm -> whnormalize gamma (unVarFromCtx <$> _leftDivided'content leftDividedTerm)
+whnormalizeNV gamma (TermSmartElim eliminee eliminators result) = whnormalize gamma result
+whnormalizeNV gamma (TermGoal str result) = whnormalize gamma result
+whnormalizeNV gamma t@(TermProblem _) = return $ Expr3 t
 
-whnormalize :: (Functor mode, Functor modty, CanSwallow (Term mode modty) mode, CanSwallow (Term mode modty) modty) =>
+whnormalize :: Multimode mode modty =>
   Ctx Type mode modty v Void ->
-  mode v -> 
   Term mode modty v ->
   Writer [Int] (Term mode modty v)
-whnormalize gamma d (Var3 v) = return $ Var3 v
-whnormalize gamma d (Expr3 t) = whnormalizeNV gamma d t
+whnormalize gamma (Var3 v) = return $ Var3 v
+whnormalize gamma (Expr3 t) = whnormalizeNV gamma t
