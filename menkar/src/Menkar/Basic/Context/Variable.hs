6ddbcb1524ce6e091716a1108d968cb47337f00d
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2018-11-27 17:58:20 +0100

    De Bruijn levels.

--- menkar/src/Menkar/Basic/Context/Variable.hs
+++ menkar/src/Menkar/Basic/Context/Variable.hs
@@ -5,6 +5,8 @@ module Menkar.Basic.Context.Variable where
 import Data.Bifunctor
 import Control.Exception.AssertFalse
 import Data.Void
+import Data.Number.Nat
+import Data.Proxy
 
 --data VarWkn v = VarLast | VarWkn v deriving (Show, Functor, Foldable, Traversable)
 newtype VarExt v = VarExt {runVarWkn :: Maybe v}
@@ -39,3 +41,42 @@ varLeftEat _ = unreachable
 
 newtype VarInModule v = VarInModule {runVarInModule :: v}
   deriving (Show, Functor, Foldable, Traversable, Eq)
+
+-------------------------------------------------
+
+class DeBruijnLevel v where
+  size :: Proxy v -> Nat
+  getDeBruijnLevel :: Proxy v -> v -> Nat
+  forDeBruijnLevel :: Proxy v -> Nat -> Maybe v
+
+instance DeBruijnLevel Void where
+  size p = 0
+  getDeBruijnLevel p = absurd
+  forDeBruijnLevel p n = Nothing
+
+proxyUnVarWkn :: Proxy (VarExt v) -> Proxy v
+proxyUnVarWkn Proxy = Proxy
+instance DeBruijnLevel v => DeBruijnLevel (VarExt v) where
+  size p = size (proxyUnVarWkn p) + 1
+  getDeBruijnLevel p (VarWkn v) = getDeBruijnLevel Proxy v
+  getDeBruijnLevel p VarLast = size p - 1
+  getDeBruijnLevel p _ = unreachable
+  forDeBruijnLevel p n
+    | n == size p - 1 = Just VarLast
+    | otherwise = VarWkn <$> forDeBruijnLevel Proxy n
+
+proxyUnVarLeftWkn :: Proxy (VarLeftExt v) -> Proxy v
+proxyUnVarLeftWkn Proxy = Proxy
+instance DeBruijnLevel v => DeBruijnLevel (VarLeftExt v) where
+  size p = size (proxyUnVarLeftWkn p) + 1
+  getDeBruijnLevel p (VarLeftWkn v) = 1 + getDeBruijnLevel Proxy v
+  getDeBruijnLevel p VarFirst = 0
+  getDeBruijnLevel p _ = unreachable
+  forDeBruijnLevel p n
+    | n == 0 = Just VarFirst
+    | otherwise = VarLeftWkn <$> forDeBruijnLevel Proxy (n - 1)
+
+instance DeBruijnLevel v => DeBruijnLevel (VarInModule v) where
+  size p = size $ runVarInModule <$> p
+  getDeBruijnLevel p (VarInModule v) = getDeBruijnLevel Proxy v
+  forDeBruijnLevel p n = VarInModule <$> forDeBruijnLevel Proxy n
