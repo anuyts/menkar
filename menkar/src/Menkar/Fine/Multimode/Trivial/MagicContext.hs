0e14e0ca4d042da8407c22b808a167ef25fe753b
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-01-09 14:57:59 +0100

    MagicContext: UniHS.

--- menkar/src/Menkar/Fine/Multimode/Trivial/MagicContext.hs
+++ menkar/src/Menkar/Fine/Multimode/Trivial/MagicContext.hs
@@ -113,111 +113,26 @@ valIndNat = val NonOp "indNat" $
     tyCS :: DeBruijnLevel v => UniHSConstructor U1 U1 v
     tyCS = pi (segEx NonOp "n" $ hs2type NatType) (hs2type $ tyCS' $ Var3 $ VarLast)
 
+{- | @UniHS {l : Nat} : UniHS (suc l) = UniHS l@
+-}
+valUniHS :: Entry U1 U1 Void
+valUniHS = val NonOp "UniHS" $
+  segEx NonOp "l" {- var 0 -} (hs2type NatType) :|-
+  Telescoped (
+    ValRHS
+      (hs2term $ UniHS U1 $ var 0)
+      (hs2term $ UniHS U1 $ Expr3 $ TermCons $ ConsSuc $ var 0)
+  )
+
+----------------------------------------------
+
 magicEntries :: [Entry U1 U1 Void]
 magicEntries = [
     valNat,
     valSuc,
-    valIndNat
-
-    {-
-    {- indNat {~ | l : Nat} {C : {n : Nat} -> Set l} {cz : C 0} {cs : {n : Nat} -> C n -> C (suc n)} {n : Nat} : C n
-         = indNat (n > C n) cz (n > ihyp > cs n ihyp) n
-    -}
-    EntryVal $ Declaration
-      (DeclNameVal $ Name NonOp "indNat")
-      trivModedModality
-      Explicit
-      $ Declaration
-          (DeclNameSegment $ Just $ Name NonOp "l")
-          trivModedModality
-          Implicit
-          (Type $ Expr3 $ TermCons $ ConsUniHS $ NatType)
-        :|-
-        Declaration
-          (DeclNameSegment $ Just $ Name NonOp "C")
-          trivModedModality
-          Explicit
-          (Type $ Expr3 $ TermCons $ ConsUniHS $ tyNatMotive)
-        :|-
-        Declaration
-          (DeclNameSegment $ Just $ Name NonOp "cz")
-          trivModedModality
-          Explicit
-          (Type $ appMotive (Var3 VarLast) $ Expr3 $ TermCons $ ConsZero)
-        :|-
-        Declaration
-          (DeclNameSegment $ Just $ Name NonOp "cs")
-          trivModedModality
-          Explicit
-          (Type $ Expr3 $ TermCons $ ConsUniHS $ Pi $ Binding
-            (Declaration
-              (DeclNameSegment $ Just $ Name NonOp "n")
-              trivModedModality
-              Explicit
-              (Type $ Expr3 $ TermCons $ ConsUniHS $ NatType)
-            )
-            (Expr3 $ TermCons $ ConsUniHS $ Pi $ Binding
-              (Declaration
-                (DeclNameSegment $ Just $ Name NonOp "ihyp")
-                trivModedModality
-                Explicit
-                (Type $ appMotive (Var3 $ VarWkn $ VarWkn $ VarLast) $ Var3 $ VarLast)
-              )
-              (appMotive (Var3 $ VarWkn $ VarWkn $ VarWkn $ VarLast) $ Expr3 $ TermCons $ ConsSuc $ Var3 $ VarWkn $ VarLast)
-            )
-          )
-        :|-
-        Declaration
-          (DeclNameSegment $ Just $ Name NonOp "n")
-          trivModedModality
-          Implicit
-          (Type $ Expr3 $ TermCons $ ConsUniHS $ NatType)
-        :|- Telescoped (
-            ValRHS
-              (Expr3 $ TermElim
-                trivModedModality
-                (Var3 $ VarLast)
-                (NatType)
-                (ElimDep
-                  (NamedBinding (Just $ Name NonOp "n") $
-                    Type $ appMotive (Var3 $ VarWkn $ VarWkn $ VarWkn $ VarLast) $ Var3 VarLast
-                  )
-                  (ElimNat
-                    (Var3 $ VarWkn $ VarWkn $ VarLast)
-                    (NamedBinding (Just $ Name NonOp "n") $
-                     NamedBinding (Just $ Name NonOp "ihyp") $
-                     Expr3 $ TermElim
-                       trivModedModality
-                       (Expr3 $ TermElim
-                         trivModedModality
-                         (Var3 $ VarWkn $ VarWkn $ VarWkn $ VarLast)
-                         _
-                         $ App $ Var3 $ VarWkn $ VarLast
-                       )
-                       _
-                       $ App $ Var3 $ VarLast
-                    )
-                  )
-                )
-              )
-              (Type $ appMotive (Var3 $ VarWkn $ VarWkn $ VarWkn $ VarLast) $ Var3 $ VarLast)
-          )
--}
+    valIndNat, -- doesn't type-check
+    valUniHS,
   ]
-  where
-    tyNatMotive = Pi $ Binding
-            (Declaration
-              (DeclNameSegment $ Just $ Name NonOp "n")
-              trivModedModality
-              Explicit
-              (Type $ Expr3 $ TermCons $ ConsUniHS $ NatType)
-            )
-            (Expr3 $ TermCons $ ConsUniHS $ UniHS U1 $ Var3 VarLast)
-    appMotive vMotive n = Expr3 $ TermElim
-            trivModedModality
-            vMotive
-            tyNatMotive
-            (App $ n)
 
 magicContext :: Ctx Type U1 U1 (VarInModule Void) Void
 magicContext = CtxEmpty U1 :<...> ModuleRHS (absurd <$> (Compose $ reverse magicEntries))
