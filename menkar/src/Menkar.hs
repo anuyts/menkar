d79aab96d9f98b40a939b508e9d47f9bf6466c76
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-03-11 15:33:42 +0100

    Adapt Menkar.hs

--- menkar/src/Menkar.hs
+++ menkar/src/Menkar.hs
@@ -1,15 +1,27 @@
 module Menkar (
   module Menkar.Basic,
   module Raw,
-  module Menkar.Fine,
   module Sc,
+  module Menkar.Fine,
+  module Menkar.WHN,
   module Menkar.TC,
-  module Menkar.PrettyPrint
+  module Menkar.PrettyPrint,
+  module Menkar.Monad,
+  module Menkar.Monads,
+  module Menkar.System,
+  module Menkar.Systems
   ) where
 
 import Menkar.Basic
 import qualified Menkar.Raw as Raw
-import Menkar.Fine
 import qualified Menkar.Scoper as Sc
+import Menkar.Fine
+import Menkar.WHN
 import Menkar.TC
+
 import Menkar.PrettyPrint
+
+import Menkar.Monad
+import Menkar.Monads
+import Menkar.System
+import Menkar.Systems
