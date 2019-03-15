9ab698699f32c20da4181e1ddf9c76c571f35db4
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-03-15 17:19:20 +0100

    Adapt Systems.Trivial.

--- menkar/src/Menkar/PrettyPrint/Fine.hs
+++ menkar/src/Menkar/PrettyPrint/Fine.hs
@@ -1,9 +1,11 @@
 module Menkar.PrettyPrint.Fine (
+  module Menkar.PrettyPrint.Fine.Class,
   module Menkar.PrettyPrint.Fine.Syntax,
   module Menkar.PrettyPrint.Fine.Context,
   module Menkar.PrettyPrint.Fine.Judgement
   ) where
 
+import Menkar.PrettyPrint.Fine.Class
 import Menkar.PrettyPrint.Fine.Syntax
 import Menkar.PrettyPrint.Fine.Context
 import Menkar.PrettyPrint.Fine.Judgement
--- menkar/src/Menkar/Systems/Trivial/Fine.hs
+++ menkar/src/Menkar/Systems/Trivial/Fine.hs
@@ -1,23 +1,23 @@
 module Menkar.Systems.Trivial.Fine where
 
 import Menkar.Fine.Syntax
-import Menkar.Fine.Multimode
-import Menkar.PrettyPrint.Fine.Syntax
+import Menkar.System.Fine
+import Menkar.PrettyPrint.Fine
 
 import Text.PrettyPrint.Tree
 
-import GHC.Generics (U1 (..))
+import GHC.Generics (U1 (..), V1 (..))
 
 data Trivial :: KSys where
 
 type instance Mode Trivial = U1
 type instance Modality Trivial = U1
 type instance Degree Trivial = U1
+type instance SysTerm Trivial = V1
 
 instance SysTrav Trivial where
 
 instance SysSyntax (Term Trivial) Trivial where
-
   
 instance Fine2Pretty Trivial U1 where
   fine2pretty gamma U1 opts = ribbon "*"
@@ -27,20 +27,17 @@ instance Fine2Pretty Trivial U1 where
 instance Multimode Trivial where
   idMod U1 = U1
   compMod U1 U1 U1 = U1
-  wildMode = U1
-  wildModty = U1
-  flatMod = U1
-  irrMod = U1
+  divMod (ModedModality U1 U1) (ModedModality U1 U1) = U1
+  crispMod = U1
   dataMode = U1
   approxLeftAdjointProj (ModedModality U1 U1) U1 = U1
   sigmaHasEta (ModedModality U1 U1) U1 = True
-  divModedModality (ModedModality U1 U1) (ModedModality U1 U1) = (ModedModality U1 U1)
+  term2mode t = U1
+  term2modty t = U1
 
 trivModedModality = ModedModality U1 U1
 
 instance Degrees Trivial where
   eqDeg = U1
-  topDeg = U1
+  topDeg = Nothing
   divDeg (ModedModality U1 U1) U1 = U1
-  isTopDeg U1 = False
-  isEqDeg U1 = True
