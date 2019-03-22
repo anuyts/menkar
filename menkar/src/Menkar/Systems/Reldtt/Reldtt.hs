a83877abdc770be2c3785d8edef58d288f35d58c
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-03-22 15:30:44 +0100

    Reldtt: SysTrav.

--- menkar/src/Menkar/Systems/Reldtt/Reldtt.hs
+++ menkar/src/Menkar/Systems/Reldtt/Reldtt.hs
@@ -3,15 +3,21 @@ module Menkar.Systems.Reldtt.Reldtt where
 import Menkar.Fine
 import Menkar.System
 
+import GHC.Generics
+
 data Reldtt :: KSys where
 
 type instance Mode Reldtt = ReldttMode
 type instance Modality Reldtt = ReldttModality
-type instance Degree Reldtt = ReldttTerm
+type instance Degree Reldtt = ReldttDegree
+type instance SysTerm Reldtt = ReldttTerm
 
 newtype ReldttMode v = ReldttMode (Term Reldtt v)
+  deriving (Functor, Foldable, Traversable, Generic)
 newtype ReldttModality v = ReldttModality (Term Reldtt v)
+  deriving (Functor, Foldable, Traversable, Generic)
 newtype ReldttDegree v = ReldttDegree (Term Reldtt v)
+  deriving (Functor, Foldable, Traversable, Generic)
 
 data ModtyTerm v =
   ModtyTermId (ReldttMode v) {-^ The mode -} |
@@ -19,9 +25,15 @@ data ModtyTerm v =
   ModtyTermPar (ReldttMode v) {-^ The codomain -} |
   ModtyTermDisc (ReldttMode v) {-^ The domain -} |
   ModtyTermPrep0 (ReldttModality v) {-^ The argument modality -} |
-  ModtyUnavailable (ReldttMode v) {-^ The domain -} (ReldttMode v) {-^ The codomain; only for prettyprinting. -} |
-  ModtyApproxLeftAdjointProj (ReldttModality v) {-^ The argument modality -}
+  {-| Only for prettyprinting. -} 
+  ModtyUnavailable (ReldttMode v) {-^ The domain -} (ReldttMode v) {-^ The codomain. -} |
+  ModtyApproxLeftAdjointProj (ReldttModality v) {-^ The argument modality -} |
+  ModtyAbs (NamedBinding Term Reldtt v)
+  deriving (Functor, Foldable, Traversable, Generic)
 
 data ReldttTerm v =
   TermModty (ModtyTerm v)
+  deriving (Functor, Foldable, Traversable, Generic)
+
+instance SysTrav Reldtt where
   
