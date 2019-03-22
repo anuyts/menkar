0aac95d0533076195c1fd1bd6bc97846c6100630
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-03-22 16:21:15 +0100

    Reldtt: SysSyntax.

--- menkar/src/Menkar/Systems/Reldtt/Reldtt.hs
+++ menkar/src/Menkar/Systems/Reldtt/Reldtt.hs
@@ -13,27 +13,43 @@ type instance Degree Reldtt = ReldttDegree
 type instance SysTerm Reldtt = ReldttTerm
 
 newtype ReldttMode v = ReldttMode (Term Reldtt v)
-  deriving (Functor, Foldable, Traversable, Generic)
+  deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
 newtype ReldttModality v = ReldttModality (Term Reldtt v)
-  deriving (Functor, Foldable, Traversable, Generic)
+  deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
 newtype ReldttDegree v = ReldttDegree (Term Reldtt v)
-  deriving (Functor, Foldable, Traversable, Generic)
+  deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
 
 data ModtyTerm v =
+  ModtyTermIdSet {-^ The modality @<> : 0 -> 0@, i.e. the identity functor on the category of sets. -} |
   ModtyTermId (ReldttMode v) {-^ The mode -} |
   ModtyTermComp (ReldttModality v) (ReldttModality v) {-^ Mathematical order -} |
-  ModtyTermPar (ReldttMode v) {-^ The codomain -} |
-  ModtyTermDisc (ReldttMode v) {-^ The domain -} |
-  ModtyTermPrep0 (ReldttModality v) {-^ The argument modality -} |
+  --ModtyTermPar (ReldttMode v) {-^ The codomain -} |
+  --ModtyTermDisc (ReldttMode v) {-^ The domain -} |
+  --ModtyTermPrep0 (ReldttModality v) {-^ The argument modality -} |
   {-| Only for prettyprinting. -} 
   ModtyUnavailable (ReldttMode v) {-^ The domain -} (ReldttMode v) {-^ The codomain. -} |
   ModtyApproxLeftAdjointProj (ReldttModality v) {-^ The argument modality -} |
-  ModtyAbs (NamedBinding Term Reldtt v)
-  deriving (Functor, Foldable, Traversable, Generic)
+  {-| @'ModtyPrep' d mu@ represents @<d, 0*mu, ..., n*mu>@.
+      Forbidden for combinations that after reduction might create non-monotonous modalities. -}
+  ModtyPrep (ReldttDegree v) (ReldttModality v) |
+  {-| Forbidden for expressions that might reduce to something non-monotonous. -}
+  ModtyAbs (ReldttMode v) {-^ The domain -} (ReldttMode v) {-^ The codomain. -} (NamedBinding Term Reldtt v)
+  deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
+
+data DegTerm v =
+  DegEq |
+  DegZero |
+  {-| Forbidden for terms that might reduce to Top. -}
+  DegSuc (ReldttDegree v) |
+  DegTop |
+  DegGet (ReldttDegree v) (ReldttModality v)
+  deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
 
 data ReldttTerm v =
-  TermModty (ModtyTerm v)
-  deriving (Functor, Foldable, Traversable, Generic)
+  TermModty (ModtyTerm v) |
+  TermDeg (DegTerm v)
+  deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
 
 instance SysTrav Reldtt where
   
+instance SysSyntax (Term Reldtt) Reldtt
