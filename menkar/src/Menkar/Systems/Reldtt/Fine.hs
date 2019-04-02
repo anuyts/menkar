f72b34c69d4d281bc30490c76e0fa3494c746905
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-04-02 10:17:39 +0200

    Reldtt: Reimplement Fine.

--- menkar/src/Menkar/Systems/Reldtt/Fine.hs
+++ menkar/src/Menkar/Systems/Reldtt/Fine.hs
@@ -14,19 +14,50 @@ type instance SysTerm Reldtt = ReldttTerm
 
 {-| @ReldttModeOne@ and @ReldttModeNull@ are really just modes 1 and 0 (depth 0 and -1) but with a special treatment.
 -}
-data ReldttMode v = ReldttMode (Term Reldtt v) | ReldttModeOne | ReldttModeNull
+data ReldttMode v = ReldttMode {unMode :: Term Reldtt v}
   deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
 newtype ReldttModality v = ReldttModality {unModality :: Term Reldtt v}
   deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
 newtype ReldttDegree v = ReldttDegree {unDegree :: Term Reldtt v}
   deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
 
-pattern BareMode d = ReldttMode (Expr2 (TermCons d))
+pattern BareMode d = ReldttMode (Expr2 (TermSys (TermMode d)))
+pattern BareFinMode d = BareMode (ModeTermFinite (Expr2 (TermCons d)))
 pattern BareModty mu = ReldttModality (Expr2 (TermSys (TermModty mu)))
 pattern BareDeg i = ReldttDegree (Expr2 (TermSys (TermDeg i)))
 
+data ModeTerm v = ModeTermFinite (Term Reldtt v) | ModeTermOmega
+  deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
+
+data KnownDeg = KnownDegEq | KnownDeg Int | KnownDegTop
+data KnownModty = KnownModty Int {-^ Domain -} Int {-^ Codomain -} [KnownDeg]
+data ModtyTail v =
+  TailEmpty |
+
+  TailDisc   (ReldttMode v) {-^ Tail codomain -} |
+  TailCodisc (ReldttMode v) {-^ Tail codomain -} |
+
+  TailForget (ReldttMode v) {-^ Tail domain -} |
+
+  TailDiscForget   (ReldttMode v) {-^ Tail domain -} (ReldttMode v) {-^ Tail codomain -} |
+  TailCodiscForget (ReldttMode v) {-^ Tail domain -} (ReldttMode v) {-^ Tail codomain -} |
+  TailCont (ReldttMode v)
+  deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
+
 data ModtyTerm v =
+  ModtyTerm KnownModty (ModtyTail v) |
   
+  {-| If @mu : d1 -> d2@ and @nu : d2 -> d3@, then the composite is @'ModtyTermComp' nu d2 mu@ -}
+  ModtyTermComp (ReldttModality v) (ReldttMode v) (ReldttModality v) |
+  {-| Only for prettyprinting. -} 
+  ModtyTermDiv (ReldttModality v) (ReldttModality v) |
+  ModtyApproxLeftAdjointProj (ReldttModality v) {-^ The argument modality -} |
+  
+  {-| Only for prettyprinting. -} 
+  ModtyUnavailable (ReldttMode v) {-^ The domain -} (ReldttMode v) {-^ The codomain. -}
+  deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
+
+  {-
   {-| @ModtyTermCrisp ddom dcod@ is @< = | =, ..., = > : ddom -> dcod@ -}
   ModtyTermCrisp (Term Reldtt v) (Term Reldtt v) |
   {-| @ModtyTermDiscPar d m n@ is @disc_0^n . fget_0^m = < = | =^n, m, m+1, ..., m+d-1> : m+d -> n+d@ -}
@@ -83,6 +114,7 @@ data ModtyTerm v =
   
   {-| Only for prettyprinting. -} 
   ModtyUnavailable (ReldttMode v) {-^ The domain -} (ReldttMode v) {-^ The codomain. -}
+  -}
   
   
   {-
@@ -105,7 +137,6 @@ data ModtyTerm v =
   {-| Forbidden for expressions that might reduce to something non-monotonous. -}
   ModtyAbs (ReldttMode v) {-^ The domain -} (ReldttMode v) {-^ The codomain. -} (NamedBinding Term Reldtt v)
   -}
-  deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
 
 {-
 _modtyTerm'dom :: ModtyTerm v -> ReldttMode v
@@ -139,12 +170,13 @@ data DegTerm v =
   deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
 
 data ReldttTerm v =
+  TermMode (ModeTerm v) |
   TermModty (ModtyTerm v) |
   TermDeg (DegTerm v) |
+  {-| Type of modes. -}
+  TermTyMode |
   {-| Type of degrees. -}
   TermTyDeg (ReldttMode v) |
-  --{-| Type of premodalities (non-cwf-morphisms, only for variables of closed types). -}
-  --TermTyPremodty (ReldttMode v) (ReldttMode v) |
   {-| Type of modalities. -}
   TermTyModty (ReldttMode v) (ReldttMode v)
   deriving (Functor, Foldable, Traversable, Generic1, CanSwallow (Term Reldtt))
@@ -156,6 +188,14 @@ instance SysTrav Reldtt where
 instance SysSyntax (Term Reldtt) Reldtt
 
 instance Multimode Reldtt where
+  idMod d = BareModty $ ModtyTerm (KnownModty 0 0 []) $ TailCont d
+  compMod mu2 dmid mu1 = BareModty $ ModtyTermComp mu2 dmid mu1
+  divMod (ModedModality d' mu') (ModedModality d mu) = BareModty $ ModtyTermDiv mu' mu
+  crispMod d = BareModty $ ModtyTerm (KnownModty 0 0 []) $ TailDisc d
+  dataMode = BareFinMode $ ConsZero
+  approxLeftAdjointProj (ModedModality d mu) dcod = BareModty $ ModtyApproxLeftAdjointProj mu
+
+{-
   idMod (ReldttModeNull) = BareModty $ ModtyTermNullNull
   idMod (ReldttModeOne) = BareModty $ ModtyTermContOneOne
   idMod (ReldttMode d) = BareModty $ ModtyTermDiscPar d (Expr2 $ TermCons $ ConsZero) (Expr2 $ TermCons $ ConsZero)
@@ -166,7 +206,8 @@ instance Multimode Reldtt where
   crispMod (ReldttMode d) = BareModty $ ModtyTermCrispNull d
   dataMode = ReldttModeNull
   approxLeftAdjointProj (ModedModality d mu) dcod = BareModty $ ModtyApproxLeftAdjointProj mu
-  
+-}
+
 instance Degrees Reldtt where
   eqDeg = BareDeg $ DegEq
   maybeTopDeg = Just $ BareDeg $ DegTop
