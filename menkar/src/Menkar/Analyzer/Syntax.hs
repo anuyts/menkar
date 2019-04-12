3ab91b8af425eea65decfbb7b95f2d367959486f
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-04-12 18:57:53 +0200

    Messing with type-variable abstraction.

--- menkar/src/Menkar/Analyzer/Syntax.hs
+++ menkar/src/Menkar/Analyzer/Syntax.hs
@@ -15,6 +15,7 @@ import Data.Functor.Const
 import Control.Lens
 import Data.Functor.Compose
 import Control.Monad
+import Data.Void
 
 -------------------------
 
@@ -28,12 +29,11 @@ instance (SysAnalyzer sys) => Analyzable sys (ModedModality sys) where
     return $ case token of
         TokenSubterms -> Box1 $ ModedModality (unbox1 rddom) (unbox1 rdcod) (unbox1 rmu)
         TokenTypes -> BoxClassif $ ddom :*: dcod
+        TokenRelate -> Unit2
 
 instance (SysAnalyzer sys,
           Analyzable sys (rhs sys),
-          Analyzable sys (Segment Type sys),
-          Relation (rhs sys) ~ ModedDegree sys,
-          Relation (Segment Type sys) ~ ModedDegree sys
+          Relation (rhs sys) ~ ModedDegree sys
          ) => Analyzable sys (Binding Type rhs sys) where
   type Classif (Binding Type rhs sys) = Classif (Segment Type sys) :*: (Classif (rhs sys) :.: VarExt)
   type Relation (Binding Type rhs sys) = ModedDegree sys
@@ -49,16 +49,13 @@ instance (SysAnalyzer sys,
       TokenTypes -> BoxClassif $ unboxClassif rseg :*: Comp1 (unboxClassif rbody)
       TokenRelate -> Unit2
 
-instance (SysAnalyzer sys,
-          Analyzable sys (Segment Type sys),
-          Classif (Term sys) ~ Type sys,
-          Relation (Segment Type sys) ~ ModedDegree sys
-         ) => Analyzable sys (UniHSConstructor sys) where
+instance (SysAnalyzer sys) => Analyzable sys (UniHSConstructor sys) where
   
   type Classif (UniHSConstructor sys) = Compose Maybe (Mode sys) -- a mode if you care
   type Relation (UniHSConstructor sys) = ModedDegree sys
   
-  analyze token fromType h gamma (MaybeClassified ty maybeMaybeD maybeDDeg) = Just $ do
+  analyze (token :: AnalyzerToken option) fromType h
+    (gamma :: Ctx lhs sys v Void) (MaybeClassified ty maybeMaybeD maybeDDeg) = Just $ do
     
     let dgamma' = ctx'mode gamma
     let dgamma = unVarFromCtx <$> dgamma'
@@ -74,22 +71,41 @@ instance (SysAnalyzer sys,
           TokenTypes -> BoxClassif $ Compose $ Just $ d
           TokenRelate -> Unit2
 
-      Pi binding -> do
-        let bindingClassif = case maybeMaybeD of
-              Nothing -> Nothing
-              Just (Compose Nothing) -> Nothing
-              Just (Compose (Just d)) -> Just $ _ :*: Comp1 (hs2type $ UniHS $ VarWkn <$> d)
-        rbinding <- h id gamma
-          (MaybeClassified binding bindingClassif maybeDDeg)
-          (AddressInfo ["binding"] False)
-        return $ case token of
-          TokenSubterms -> Box1 $ Pi $ unbox1 rbinding
-          TokenTypes -> BoxClassif $ Compose Nothing
-          TokenRelate -> Unit2
+      Pi binding -> handleBinder Pi binding
+
+      Sigma binding -> handleBinder Sigma binding
+
+      EmptyType -> handleConstant
+
+    where handleBinder ::
+            (Binding Type Term sys v -> UniHSConstructor sys v) -> Binding Type Term sys v ->
+            _ (AnalyzerResult option (UniHSConstructor sys) v)
+          handleBinder binder binding = do
+            let bindingClassif = case maybeMaybeD of
+                  Nothing -> Nothing
+                  Just (Compose Nothing) -> Nothing
+                  Just (Compose (Just d)) -> Just $ U1 :*: Comp1 (hs2type $ UniHS $ VarWkn <$> d)
+            rbinding <- h id gamma
+              (MaybeClassified binding bindingClassif maybeDDeg)
+              (AddressInfo ["binding"] False)
+            return $ case token of
+              TokenSubterms -> Box1 $ binder $ unbox1 rbinding
+              TokenTypes -> BoxClassif $ Compose Nothing
+              TokenRelate -> Unit2
+              
+          handleConstant = pure $ case token of
+            TokenSubterms -> Box1 $ ty
+            TokenTypes -> BoxClassif $ Compose Nothing
+            TokenRelate -> Unit2
 
 instance SysAnalyzer sys => Analyzable sys (Type sys) where
+  type Classif (Type sys) = U1
   type Relation (Type sys) = ModedDegree sys
 
 instance SysAnalyzer sys => Analyzable sys (Term sys) where
   type Classif (Term sys) = Type sys
   type Relation (Term sys) = ModedDegree sys
+
+instance SysAnalyzer sys => Analyzable sys (Segment Type sys) where
+  type Classif (Segment Type sys) = Classif (Type sys)
+  type Relation (Segment Type sys) = ModedDegree sys
