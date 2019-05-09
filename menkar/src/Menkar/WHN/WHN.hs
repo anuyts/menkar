7cdec6c534f747aa025a9d2503992e9627a16460
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-05-09 15:00:30 +0200

    Menkar.WHN.WHN: small fix.

--- menkar/src/Menkar/WHN/WHN.hs
+++ menkar/src/Menkar/WHN/WHN.hs
@@ -252,8 +252,7 @@ whnormalizeAST :: forall sys whn v t .
    MonadWHN sys whn,
    DeBruijnLevel v,
    MonadWriter [Int] whn,
-   Analyzable sys t,
-   Analyzable sys (Classif t)) => 
+   Analyzable sys t) => 
   Constraint sys ->
   Ctx Type sys v Void ->
   t v ->
