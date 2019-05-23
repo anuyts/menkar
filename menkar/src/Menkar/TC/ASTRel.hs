d3ea825b5f00c9e889e392523ea22c33d5aa3aa6
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-05-23 17:52:28 +0200

    Menkar.TC.ASTRel: distinguish by boringness.

--- menkar/src/Menkar/TC/ASTRel.hs
+++ menkar/src/Menkar/TC/ASTRel.hs
@@ -48,14 +48,28 @@ checkASTRel' parent eta relT gamma (Twice1 t1 t2) (Twice1 extraT1 extraT2) maybe
           let relS = extractRel relT
           let gammadelta = fromMaybe unreachable $ extCtx TokenTrue gamma inputT1 (conditional inputT2)
               -- Cannot fail because we already know that the shapes of t1 and t2 match.
-          addNewConstraint
-            (JudRel (analyzableToken @sys @s) (Eta True) relS gammadelta
-              (Twice1 s1 s2)
-              (Twice1 extraS1 extraS2)
-              (Twice1 <$> maybeCS1 <*> maybeCS2)
-            )
-            (Just parent)
-            ("Relating:" ++ (join $ (" > " ++ ) <$> _addressInfo'address addressInfo))
+          let eta = if _addressInfo'shouldWHN addressInfo then Eta False else Eta True
+          let maybeCSs = classifMust2will $ Twice1 <$> maybeCS1 <*> maybeCS2
+          case _addressInfo'boredom addressInfo of
+            EntirelyBoring -> checkASTRel parent eta relS gammadelta (Twice1 s1 s2) (Twice1 extraS1 extraS2) maybeCSs
+            WorthMentioning -> do
+              virtualConstraint <- defConstraint
+                (JudRel (analyzableToken @sys @s) (Eta True) relS gammadelta
+                  (Twice1 s1 s2)
+                  (Twice1 extraS1 extraS2)
+                  (classifMust2will $ Twice1 <$> maybeCS1 <*> maybeCS2)
+                )
+                (Just parent)
+                ("Relating:" ++ (join $ (" > " ++ ) <$> _addressInfo'address addressInfo))
+              checkASTRel virtualConstraint eta relS gammadelta (Twice1 s1 s2) (Twice1 extraS1 extraS2) maybeCSs
+            WorthScheduling -> addNewConstraint
+              (JudRel (analyzableToken @sys @s) (Eta True) relS gammadelta
+                (Twice1 s1 s2)
+                (Twice1 extraS1 extraS2)
+                (classifMust2will $ Twice1 <$> maybeCS1 <*> maybeCS2)
+              )
+              (Just parent)
+              ("Relating:" ++ (join $ (" > " ++ ) <$> _addressInfo'address addressInfo))
           return AnalysisRel
   case attempt of
     Right AnalysisRel -> return ()
