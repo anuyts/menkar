f3e94619f15ee098ccb6838ae970ec93cb7334a8
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-05-31 18:01:36 +0200

    Menkar.System.Scoper: hiding parent.

--- menkar/src/Menkar/System/Scoper.hs
+++ menkar/src/Menkar/System/Scoper.hs
@@ -14,13 +14,12 @@ class SysSyntax (Term sys) sys => SysScoper sys where
   scopeAnnotation :: (MonadScoper sys sc, DeBruijnLevel v) => Ctx Type sys v Void -> 
     Raw.Qualified String -> Maybe (Raw.Expr) -> sc (Annotation sys v)
   newMetaModeNoCheck :: (MonadScoper sys sc, DeBruijnLevel v) =>
-    Maybe (Constraint sys) -> Ctx Type sys v Void -> String -> sc (Mode sys v)
+    Ctx Type sys v Void -> String -> sc (Mode sys v)
   newMetaModtyNoCheck :: (MonadScoper sys sc, DeBruijnLevel v) =>
-    Maybe (Constraint sys) -> Ctx Type sys v Void -> String -> sc (Modality sys v)
+    Ctx Type sys v Void -> String -> sc (Modality sys v)
   newMetaClassif4sysASTNoCheck :: forall sc t v .
     (MonadScoper sys sc, DeBruijnLevel v, SysAnalyzer sys, Analyzable sys t) =>
     SysAnalyzableToken sys t ->
-    Maybe (Constraint sys) ->
     Ctx Type sys v Void ->
     t v ->
     ClassifExtraInput t v ->
@@ -28,33 +27,31 @@ class SysSyntax (Term sys) sys => SysScoper sys where
     sc (Classif t v)
 
 newMetaModedModalityNoCheck :: (SysScoper sys, MonadScoper sys sc, DeBruijnLevel v) =>
-  Maybe (Constraint sys) ->
   Ctx Type sys v Void ->
   String ->
   sc (ModedModality sys v)
-newMetaModedModalityNoCheck parent gamma reason = do
-  dom <- newMetaModeNoCheck parent gamma reason
-  cod <- newMetaModeNoCheck parent gamma reason
-  mu <- newMetaModtyNoCheck parent gamma reason
+newMetaModedModalityNoCheck gamma reason = do
+  dom <- newMetaModeNoCheck gamma reason
+  cod <- newMetaModeNoCheck gamma reason
+  mu <- newMetaModtyNoCheck gamma reason
   return $ ModedModality dom cod mu
 
 newMetaClassif4astNoCheck :: forall sys sc t v .
   (MonadScoper sys sc, DeBruijnLevel v, SysScoper sys, SysAnalyzer sys, Analyzable sys t) =>
   --AnalyzableToken sys t ->
-  Maybe (Constraint sys) ->
   Ctx Type sys v Void ->
   t v ->
   ClassifExtraInput t v ->
   String ->
   sc (Classif t v)
-newMetaClassif4astNoCheck maybeParent gamma t extraT reason = do
+newMetaClassif4astNoCheck gamma t extraT reason = do
   case (analyzableToken :: AnalyzableToken sys t, t) of
     (AnTokenModedModality, _) -> do
-      dom <- newMetaModeNoCheck maybeParent gamma reason
-      cod <- newMetaModeNoCheck maybeParent gamma reason
+      dom <- newMetaModeNoCheck gamma reason
+      cod <- newMetaModeNoCheck gamma reason
       return $ dom :*: cod
     (AnTokenBinding tokenRHS, Binding seg rhs) -> do
-      crhs <- newMetaClassif4astNoCheck maybeParent (gamma :.. VarFromCtx <$> seg) rhs U1 reason
+      crhs <- newMetaClassif4astNoCheck (gamma :.. VarFromCtx <$> seg) rhs U1 reason
       return $ U1 :*: (NamedBinding (getDeclNameSegment . _decl'name $ seg) $ Const1 $ crhs)
                     --ClassifBinding seg crhs
 {-    (AnTokenClassifBinding tokenRHS, ClassifBinding seg rhs) -> do
@@ -66,26 +63,26 @@ newMetaClassif4astNoCheck maybeParent gamma t extraT reason = do
       --let extraRHS = unComp1 $ snd1 extraT
       let seg :*: Comp1 extraRHS = extraT
       crhs <- newMetaClassif4astNoCheck @sys @sc @(rhs sys) @(VarExt v)
-        maybeParent (gamma :.. VarFromCtx <$> seg) rhs extraRHS reason
+        (gamma :.. VarFromCtx <$> seg) rhs extraRHS reason
       return $ NamedBinding (getDeclNameSegment . _decl'name $ seg) $ Const1 $ crhs
              --ClassifBinding seg crhs
-    (AnTokenUniHSConstructor, _) -> newMetaModeNoCheck maybeParent gamma reason
-    (AnTokenConstructorTerm, _) -> newMetaTypeNoCheck maybeParent gamma reason
+    (AnTokenUniHSConstructor, _) -> newMetaModeNoCheck gamma reason
+    (AnTokenConstructorTerm, _) -> newMetaTypeNoCheck gamma reason
     (AnTokenType, _) -> return U1
     (AnTokenDependentEliminator, _) -> return U1
-    (AnTokenEliminator, _) -> newMetaTypeNoCheck maybeParent gamma reason
-    (AnTokenTermNV, _) -> newMetaTypeNoCheck maybeParent gamma reason
-    (AnTokenTerm, _) -> newMetaTypeNoCheck maybeParent gamma reason
+    (AnTokenEliminator, _) -> newMetaTypeNoCheck gamma reason
+    (AnTokenTermNV, _) -> newMetaTypeNoCheck gamma reason
+    (AnTokenTerm, _) -> newMetaTypeNoCheck gamma reason
     (AnTokenDeclaration tokenRHS, decl) -> do
-      newMetaClassif4astNoCheck maybeParent gamma (_decl'content decl) extraT reason
+      newMetaClassif4astNoCheck gamma (_decl'content decl) extraT reason
     (AnTokenTelescoped tokenRHS, _) -> return U1
     (AnTokenValRHS, _) -> return U1
     (AnTokenModuleRHS, _) -> return U1
     (AnTokenEntry, _) -> return U1
     (AnTokenU1, _) -> return U1
     (AnTokenPair1 tokenF tokenG, fv :*: gv) ->
-      (:*:) <$> newMetaClassif4astNoCheck maybeParent gamma fv (fst1 extraT) reason
-            <*> newMetaClassif4astNoCheck maybeParent gamma gv (snd1 extraT) reason
-    (AnTokenConst1 token, Const1 t) -> newMetaClassif4astNoCheck maybeParent gamma t extraT reason
-    (AnTokenSys sysToken, _) -> newMetaClassif4sysASTNoCheck sysToken maybeParent gamma t extraT reason
+      (:*:) <$> newMetaClassif4astNoCheck gamma fv (fst1 extraT) reason
+            <*> newMetaClassif4astNoCheck gamma gv (snd1 extraT) reason
+    (AnTokenConst1 token, Const1 t) -> newMetaClassif4astNoCheck gamma t extraT reason
+    (AnTokenSys sysToken, _) -> newMetaClassif4sysASTNoCheck sysToken gamma t extraT reason
     --_ -> _newMetaClassifNoCheck
