099e3beb907e11e39616659bcf2b965c480983b0
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-08-08 16:57:52 +0200

    Functor.Coerce: Use unsafeCoerce.

--- menkar/src/Data/Functor/Coerce.hs
+++ menkar/src/Data/Functor/Coerce.hs
@@ -1,10 +1,12 @@
 module Data.Functor.Coerce where
 
 import Data.Coerce
+import Unsafe.Coerce
 
 -- | If we have quantified constraints, and can make types like Ctx representational in v, then we can make this work.
 (!<$>) :: forall f a b . (Functor f, Coercible a b) => (a -> b) -> f a -> f b
-h !<$> fa = h <$> fa
+--h !<$> fa = h <$> fa
+h !<$> fa = unsafeCoerce fa
 {-
 (!<$>) :: forall f a b . (Functor f, Coercible a b, forall x y . Coercible x y => Coercible (f x) (f y)) =>
   (a -> b) -> f a -> f b
