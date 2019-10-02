be746456e15f556a2c03ac1e2951fd904381683a
Andreas Nuyts <andreas.nuyts@cs.kuleuven.be>
2019-10-02 18:51:21 +0200

    Revert "Revert "Data.FoldCache: don't use lists.""
    This reverts commit 5816143f86049f14fb0a8171fd389682a6b6592a.

--- menkar/src/Data/FoldCache.hs
+++ menkar/src/Data/FoldCache.hs
@@ -8,29 +8,30 @@ import GHC.Generics
 {-| Use this data structure if you want to cache the heavy computation for the methods of @Foldable@.
     In fact, this is the free monad over the list monad, see Control.Monad.Free. -}
 data FoldCache x where
-  FCQuote :: x -> FoldCache x
-  FCConcat :: [FoldCache x] -> FoldCache x
+  FCEmpty :: FoldCache x
+  FCPure :: x -> FoldCache x
+  FCAppend :: FoldCache x -> FoldCache x -> FoldCache x
   deriving (Functor, Foldable, Traversable, Generic, Generic1)
 
 instance Applicative FoldCache where
-  pure = FCQuote
-  FCQuote f <*> fcx = f <$> fcx
-  FCConcat fcfs <*> fcx = FCConcat (fcfs <&> (<*> fcx))
+  pure = FCPure
+  FCEmpty <*> fcx = FCEmpty
+  FCPure f <*> fcx = f <$> fcx
+  FCAppend fcf fcg <*> fcx = FCAppend (fcf <*> fcx) (fcg <*> fcx)
 
 instance Monad FoldCache where
-  return = FCQuote
-  FCQuote x >>= h = h x
-  FCConcat fcxs >>= h = FCConcat (fcxs <&> (>>= h))
+  return = FCPure
+  FCEmpty >>= h = FCEmpty
+  FCPure x >>= h = h x
+  FCAppend fcx fcy >>= h = FCAppend (fcx >>= h) (fcy >>= h)
 
 instance Semigroup (FoldCache x) where
-  fcx <> fcy = FCConcat [fcx, fcy]
+  (<>) = FCAppend
 
 instance Monoid (FoldCache x) where
-  mempty = FCConcat []
-  mappend fcx fcy = FCConcat [fcx, fcy]
-  mconcat = FCConcat
+  mempty = FCEmpty
 
 toFoldCache :: Foldable f => f v -> FoldCache v
-toFoldCache = foldMap FCQuote
+toFoldCache = foldMap FCPure
 
 deriving instance NFData_ FoldCache
